663f518a019e2da8e0c02b698b4a0e03d82f3df0
NajwaMal <ng-7@hotmail.com>
2019-08-23 02:52:15 +0300

    variable and print new line

--- Week1.js
+++ Week1.js
@@ -1,4 +1,13 @@
 // print statement
-document.write("Hello JavaScript world!");
+document.write("Hello JavaScript world!" + '<br/>');
 // print mathematic operation
-document.write(5 + 6);
+document.write(5 + 6 + '<br/>');
+
+//day 3: Variable declaration
+var x = 10;
+//print variable
+document.write(x + '<br/>');
+
+//day 4 & 5: print string in new line 
+document.write("Hello, this is my first challenge and I will do it " + x +" times."
+                + '<br/>'+"I will do it, I will learn JavaScript.");
