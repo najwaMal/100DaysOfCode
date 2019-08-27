f3b236f30f029eea7cdeec6da5ece29d519de7b7
NajwaMal <ng-7@hotmail.com>
2019-08-27 22:35:04 +0300

    semicolons example

---
+++ Day_008.js
@@ -0,0 +1,8 @@
+document.write("Day 8: Semicolons" + '<>br/');
+
+//It is important to add semicolons here: many declarations in the same line
+var a = 6; var b =7;
+
+// Here is, the semicolon is optional but it is better to add it
+var c= 8
+var d= 9
