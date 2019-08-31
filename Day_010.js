ca3068959c6e635922005fc7cd1ab0f4c1fc5505
NajwaMal <ng-7@hotmail.com>
2019-08-31 08:31:11 +0300

    Dates and Times

---
+++ Day_010.js
@@ -0,0 +1,6 @@
+document.write("Day 10: Dates and Times" + '<br/>');
+
+// It prints the current day
+document.write(new Date());
+// It prints specific date and time
+document.write(new Date(2016, 0, 1, 17, 10, 30));
