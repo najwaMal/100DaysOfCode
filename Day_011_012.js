a4a2b45d71361b6f6fae99bd793b4bbcdf6607ca
NajwaMal <ng-7@hotmail.com>
2019-08-31 08:44:27 +0300

    Alert and Console

---
+++ Day_011_012.js
@@ -0,0 +1,7 @@
+document.write("Day 11 - 12: Alert And Consol" + '<br/>');
+// It is will pop up a message in the browser
+alert("Welcome To JavaScript world!");
+window.alert("Welcome To JavaScript world!");
+
+//To display the output in the browser console
+console.log("Welcome To JavaScript world!");
