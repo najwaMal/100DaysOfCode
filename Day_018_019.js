ffd4087b091aa7898c8ea76f8fb0040c2ec9301c
NajwaMal <ng-7@hotmail.com>
2019-09-04 10:51:46 +0300

    week3 Challenge

---
+++ Day_018_019.js
@@ -0,0 +1,13 @@
+document.write(" Days 18, 19: Switch Challenge <br/>");
+
+var browser = "Operah";
+
+if (browser == "Edge"){
+    alert( "You've got the Edge!" );
+}
+else if (browser == "Chrome" || browser == "Firefox" || browser == "Safari" || browser == "Opera"){
+    alert( 'Okay we support these browsers too' );
+}
+else{
+    alert( 'We hope that this page looks ok!' );
+}
