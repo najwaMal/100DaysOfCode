ffabac3178bdfcbe838be65ed554fb6f2d8996d6
NajwaMal <ng-7@hotmail.com>
2019-09-12 00:35:54 +0300

    Jumps- Breack

---
+++ Day_023.js
@@ -0,0 +1,12 @@
+document.write(" Day 23: Jumps </br>");
+
+var tech= ["VR", "AR", "MR"];
+var learn = "";
+
+list: {
+  learn += tech[0] + "<br>"; 
+  learn += tech[1] + "<br>"; 
+  break list;
+  learn += tech[2] + "<br>"; 
+}
+document.write(learn);
