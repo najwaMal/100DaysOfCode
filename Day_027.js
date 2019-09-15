778088b5aa5d4df01277326da54b14ce70a79eaa
NajwaMal <ng-7@hotmail.com>
2019-09-15 08:59:27 +0300

    Jump Return

---
+++ Day_027.js
@@ -0,0 +1,9 @@
+document.write("Day 27: Jump Return </br>");
+
+
+function sum(x,y){
+     document.write("the result = ")
+     return x+y; 
+     document.write(" this message wil not pint");
+} 
+document.write(sum(3,4));
