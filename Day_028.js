0664d19af0001451b1a486843768a9a76792a2a0
NajwaMal <ng-7@hotmail.com>
2019-09-16 22:47:05 +0300

    Jump throw

---
+++ Day_028.js
@@ -0,0 +1,11 @@
+document.write("Day 28: Jump throw <br/>");
+
+
+var x ; 
+function noneZero(x) { 
+if (x != 0) {
+document.write("ِAcceptable number"); 
+}
+else {throw "Zero is not acceptable "; }
+} 
+noneZero(2);
