f2adb7b5ad70d0498ef369620af2011214950874
NajwaMal <ng-7@hotmail.com>
2019-09-18 23:58:10 +0300

    Object

---
+++ Day_030.js
@@ -0,0 +1,11 @@
+document.write("Day 30: Object </br>");
+
+var person = {
+     firstName: "Najwa", 
+     lastName: "Mal", 
+     age: 25, 
+     eyeColor: "brown" 
+    };
+
+document.write(person.firstName + " ");
+document.write(person["lastName"]);
