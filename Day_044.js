08434f9b457d3fc5c851ae5e36dfccf025b4deb1
NajwaMal <ng-7@hotmail.com>
2019-10-02 20:41:16 +0300

    Creating Arrays

---
+++ Day_044.js
@@ -0,0 +1,6 @@
+document.write("Day 44: Creating Arrays </br>");
+
+//method 1 to create an array
+var ProgrammingLang = ["c++", "C#", "Java"];
+//method 2 to create an array
+var webProgammingLang = new Array ("PHP", "Java Script", "Python");
