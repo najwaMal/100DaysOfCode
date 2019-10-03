0499f0191443c937b897d51e2bdb869f1907b879
NajwaMal <ng-7@hotmail.com>
2019-10-03 09:14:08 +0300

    Accessing Array's elements

---
+++ Day_045.js
@@ -0,0 +1,5 @@
+document.write("Day 45: Reading and Accessing Array's elements </br>");
+
+var fruits = ["Apple", "pineapple", "strawberry"];
+
+document.write("My favourite fruit is " + fruits[1]);
