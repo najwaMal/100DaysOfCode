0eef73db3ac17ab074b36ae2729d42bb1b4a94ba
NajwaMal <ng-7@hotmail.com>
2019-10-07 16:44:07 +0300

    Array Length

---
+++ Day_048.js
@@ -0,0 +1,13 @@
+document.write("Day 48: Array Length </br> ");
+
+//Method 1
+var langs = new Array(3); 
+
+//Method 2
+var prog = []; 
+prog.length = 5;
+
+//Method 3
+var names = ['Omar', 'Talal', 'Ahmed'];
+
+
