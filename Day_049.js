ab271663ff1cbc975fe93417b385e8eca39abd54
NajwaMal <ng-7@hotmail.com>
2019-10-07 16:52:38 +0300

    Adding array elements

---
+++ Day_049.js
@@ -0,0 +1,17 @@
+document.write("Day 49: Adding Array Elements </br>");
+
+//Method 1
+var numbers = [];
+
+numbers [0] = 'zero';//add the first element
+numbers [1] = 'one'; //add the second element
+
+//Method 2 using push method
+numbers = []; 
+numbers.push('zero') //add the first element
+numbers.push('one', 'two') //add second and third elements
+
+//Method 3
+var names = ['Omar', 'Talal', 'Ahmed']; 
+names[3] = 'Saad'; 
+names[4] = 'Fahad';
