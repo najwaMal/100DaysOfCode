0b7678125e4fedc6f128c69288732da18c28170f
NajwaMal <ng-7@hotmail.com>
2019-11-20 08:52:06 +0300

    Week 11 challenge

---
+++ Day_074_075.js
@@ -0,0 +1,8 @@
+
+document.write("Day 74 & 75: Week 11 challenge");
+
+if (confirm("Click Ok to Open a new window or Cancel to open a new tab .\n")){
+    window.open('about:blank');
+  } else {
+    window.location = "https://www.saudidev.org";
+  }
