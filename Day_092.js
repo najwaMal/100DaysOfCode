78212e3f16808a1c10981a333ddb9ce97064adc6
NajwaMal <ng-7@hotmail.com>
2019-12-05 19:33:33 +0300

    Navigator

---
+++ Day_092.js
@@ -0,0 +1,8 @@
+document.write("Day 92: Navigator </br>");
+document.write("The browser name:" + navigator.appName + "</br>");
+document.write("The product name of the browser: "+ navigator.product + "</br>");
+document.write("The browser version:" + navigator.appVersion + "</br>");
+document.write("The operating system: :" + navigator.platform + "</br>");
+document.write("The browser language: :" + navigator.language + "</br>");
+document.write("Is the browser online?: :" + navigator.onLine + "</br>");
+
